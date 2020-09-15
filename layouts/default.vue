112a60041a20f7872c69bfb9f471100952a433a8
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-09-15 09:50:45 +0900

    Add NavBar, Create

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
+    <nav-bar />
     <Nuxt />
   </div>
 </template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+export default {
+  components: {
+    NavBar,
+  },
+}
+</script>
 
 <style>
 html {
