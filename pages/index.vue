c4d7435513e8e97c2d74e8416495a6c4ca8b79f9
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-09-19 09:16:41 +0900

    API calls with Axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
+  components: {
+    EventCard,
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time, please try again',
+        })
+      })
+  },
   // Property used by vue-meta to add header tags
   head() {
     return {
@@ -15,7 +40,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
